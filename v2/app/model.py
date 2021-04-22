b782c8d385e61f7791d267bfb8294efe814e1340
jacksonet00 <jacksoneliastaylor@gmail.com>
2021-04-22 12:04:49 -0400

    refactor model

--- v2/app/model.py
+++ v2/app/model.py
@@ -1,32 +1,41 @@
 from .engine import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
 
-class User(Base):
+class BaseModel:
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+class User(Base, BaseModel):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
-    zoos = relationship('Zoo', back_populates='owner', )
+    zoos = relationship('Zoo', back_populates='owner')
     animals = relationship('Animal', back_populates='owner')
 
 
-class Animal(Base):
+class Animal(Base, BaseModel):
     __tablename__ = 'animals'
-    id = Column(Integer, primary_key=True)
+
     name = Column(String, nullable=False)
-    owner_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    owner_id = Column(Integer, ForeignKey(
+        'users.id', ondelete='CASCADE'), nullable=False)
     owner = relationship('User', back_populates='animals')
-    zoo_id = Column(Integer, ForeignKey('zoos.id'), nullable=True)
+    zoo_id = Column(Integer, ForeignKey(
+        'zoos.id', ondelete='SET NULL'), nullable=True)
     zoo = relationship('Zoo', back_populates='animals')
 
 
-class Zoo(Base):
+class Zoo(Base, BaseModel):
     __tablename__ = 'zoos'
-    id = Column(Integer, primary_key=True)
+
     name = Column(String, unique=True, nullable=False)
-    owner_id = Column(Integer, ForeignKey('users.id'), nullable=True)
+    owner_id = Column(Integer, ForeignKey(
+        'users.id', ondelete='CASCADE'), nullable=False)
     owner = relationship('User', back_populates='zoos')
     animals = relationship('Animal', back_populates='zoo')
