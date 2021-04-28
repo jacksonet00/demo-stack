5937f672de40cab20acecde4a444ee63a60b036b
jacksonet00 <jacksoneliastaylor@gmail.com>
2021-04-28 12:59:11 -0400

    implement jwt

---
+++ v2/app/auth.py
@@ -0,0 +1,32 @@
+from datetime import (datetime, timedelta)
+import jwt
+
+key = 'asdfasdfasdfasdf'
+
+
+def cat(identity):
+    exp_date = datetime.utcnow() + timedelta(minutes=15)
+    return jwt.encode({'payload': identity, 'exp': exp_date}, key, algorithm='HS256')
+
+
+def crt(identity):
+    exp_date = datetime.utcnow() + timedelta(days=30)
+    return jwt.encode({'payload': identity, "exp": exp_date}, key, algorithm='HS256')
+
+
+def is_auth(info):
+    _jwt = info.context.headers.get('Authorization')
+    try:
+        jwt.decode(_jwt, key=key, algorithms='HS256')
+    except:
+        return False
+    return True
+
+
+def get_identity(info):
+    _jwt = info.context.headers.get('Authorization')
+    try:
+        res = jwt.decode(_jwt, key=key, algorithms='HS256')
+    except:
+        res = {'payload': ''}
+    return res['payload']
